c3d08ff4f579f2d31b63113303c39c66d842fff7
Minato30 <105573043+Minato30@users.noreply.github.com>
2022-05-24 10:07:43 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -89,12 +89,6 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="💞 ᴀʙᴏᴜᴛ ᴍᴇ​​ 💞", callback_data="fallen_"),
-        InlineKeyboardButton(
-            text="🙄 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ 🙄", callback_data="source_"),
-    ],
-    [
-        InlineKeyboardButton(
             text="💔 sᴜᴘᴘᴏʀᴛ 💔", url=f"https://t.me/{SUPPORT_CHAT}"
         ),
         InlineKeyboardButton(
